705074dd9e9b64e7523fe5007b98bfac8955eb6e
Aman Dangol <amandangol423@gmail.com>
2024-08-13 14:01:18 +0545

    contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,5 @@
+import styles from "../css_modules/contact.module.css";
+
+export default function Contact() {
+  return <a className={styles.btn} href="mailto:amandangol423@gmail.com">Contact</a>;
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import styles from "../css_modules/navbar.module.css";
 import Myname from "./Myname";
 import NavbarComponents from "./NavbarComponents";
+import Contact from "./Contact";
 export default function Navbar() {
   return (
     <div className={styles.container}>
       <Myname />
       <NavbarComponents />
+      <Contact />
     </div>
   );
 }
